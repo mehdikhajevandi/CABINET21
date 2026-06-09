1bbb0fb702a219c4287fa68553404f902ada5aee
Mehdi Khajebandiecho <mehdi.khajevandi.21@gmail.com>
2026-06-09 12:11:50 +0330

    Update button

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'tahoma' }}>
+      <h1>به وب‌سایت کابینت ۲۱ خوش آمدید</h1>
+      <p>نمونه کارها و خدمات طراحی ۳ بعدی کابینت به زودی در این مکان قرار می‌گیرد.</p>
+    </div>
+  )
 }
